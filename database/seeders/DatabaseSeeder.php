b295fcdc1c79bfd3984de349b39ce345e8ad0786
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2024-10-24 19:31:45 +0800

    add workflows

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(1)->create();
+        User::factory(1)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        Category::factory(1)->create();
+        // Category::factory(1)->create();
+        $this->call(CategorySeeder::class);
     }
 }
